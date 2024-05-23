e21a56639dcc49e437fd8170b227b13ad63f5343
samade747 <samad.e747@gmail.com>
2024-05-23 12:47:57 +0500

    6AM 23 5 2024

--- api/index.js
+++ api/index.js
@@ -4,17 +4,15 @@ import dotenv from 'dotenv';
 import { GoogleGenerativeAI } from '@google/generative-ai';
 
 dotenv.config();
-
 const app = express();
 const apiKey = process.env.API_KEY;
 
 if (!apiKey) {
-    console.error('API_KEY is not defined in the environment variables');
+    console.error('API_KEY is not defined');
     process.exit(1);
 }
 
 const genAI = new GoogleGenerativeAI(apiKey);
-
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
@@ -26,27 +24,17 @@ app.post('/', (req, res) => {
 
 app.post('/chatgpt', async (req, res) => {
     const userInput = req.body.input;
-
     if (!userInput) {
-        return res.status(400).send('No Input Provided');
+        return res.status(400).send('No Input');
     }
-
     try {
-        console.log('Generating content for input:', userInput);
-
-        
+        console.log('input:', userInput);        
         const model = genAI.getGenerativeModel({ 
             model: 'gemini-pro'
-        });
-
-        
-        const result = await model.generateContent(userInput);
-
-        
+        });        
+        const result = await model.generateContent(userInput);        
         const text = result.response.candidates[0].content;
-
         console.log('Generated text:', text);
-
         res.json({ response: text });
     } catch (error) {
         console.error('Error generating content:', error);
